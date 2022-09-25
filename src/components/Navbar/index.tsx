758090987e987d7e5872365850d6aba77b4a8f6e
Renan <renan23eduardo@gmail.com>
2022-09-24 23:56:01 -0300

    Addi00ng search bar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Search from '../Search'
 
 export default function Navbar() {
   return (
---
+++ src/components/Search/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Search() {
+  return (
+    <div className='search'>
+        <div className='searchForm'>
+            <input type='text' />
+        </div>
+        <div className="userChat">
+            <img src='https://static-wp-tor15-prd.torcedores.com/wp-content/uploads/2022/08/cristiano-ronaldo-5-590x393.jpg'  alt='' />
+            <div className="userChatInfo">
+                <span>Jane</span>
+            </div>
+        </div>
+    </div>
+  )
+}
